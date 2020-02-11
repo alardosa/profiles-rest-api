6f1c2451b55dd661e46bb580542a6b383b1c3db3
Al Ardosa <alardosa@gmail.com>
2020-02-10 18:58:02 -0800

    Create Serializer, Get and Post

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,18 +1,36 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+
+from profiles_api import serializers
 
 
 class HelloApiView(APIView):
-    """Test API Viewsss"""
+    """Test API View"""
+    serializer_class = serializers.HelloSerializer
 
     def get(self, request, format=None):
         """Returns a list of APIView features"""
-
         an_apiview = [
-            'Uses HTTP methods as functions (get, post, patch, put, delete)',
+            'Uses HTTP methods as function (get, post, patch, put, delete)',
             'Is similar to a traditional Django View',
-            'Gives you the most control over your logic',
+            'Gives you the most control over you application logic',
             'Is mapped manually to URLs',
         ]
 
         return Response({'message': 'Hello!', 'an_apiview': an_apiview})
+
+    def post(self, request):
+        """Create a hello message with our name"""
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f'Hello {name}'
+            return Response({'message': message})
+        else:
+            return Response(
+                serializer.errors,
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
