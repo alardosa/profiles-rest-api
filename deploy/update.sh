6ee9cb556a9a1450e158a6ffa0a0dc12399db6c5
alardosa <alardosa@gmail.com>
2020-02-20 14:10:19 +0800

    update setup

--- deploy/update.sh
+++ deploy/update.sh
@@ -7,6 +7,6 @@ PROJECT_BASE_PATH='/usr/local/apps/profiles-rest-api'
 git pull
 sudo $PROJECT_BASE_PATH/env/bin/python manage.py migrate
 sudo $PROJECT_BASE_PATH/env/bin/python manage.py collectstatic --noinput
-sudo supervisorctl restart profiles_api
+sudo supervisorctl reload profiles_api
 
 echo "DONE! :)";
